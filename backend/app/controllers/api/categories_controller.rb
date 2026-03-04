7c807dc9fd462f6952c8bbb33a917991488cc1ce
carlbrizuela <carljasper.brizuela@gmail.com>
2026-03-04 13:17:45 +0800

    feat: add backend create endpoint with validations
    Add POST /categories route
    
    Implement CategoriesController#create
    
    Add presence and uniqueness validations for name

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -1,6 +1,28 @@
 class Api::CategoriesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
   def index
     categories = Category.order(:name)
     render json: categories
+  end
+
+  def create
+    category = Category.new(category_params)
+
+    if category.save
+      render json: {name: category.name}, status: :created
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def record_not_unique
+    render json: { errors: ["Category already exists"] }, status: :unprocessable_entity
   end
 end
