0caefc63fd6cf83ab9971fcc063f4486a2cb23a6
carlbrizuela <carljasper.brizuela@gmail.com>
2026-03-05 14:53:34 +0800

    refactor: use shared params and merge to DRY Expense validation tests

--- backend/spec/models/expense_spec.rb
+++ backend/spec/models/expense_spec.rb
@@ -2,33 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
   let!(:transport_category) { Category.create!(name: "Transport", id: 1) }
+  let(:valid_params) do
+  {
+    description: "Bus",
+    amount: 50,
+    category_id: transport_category.id,
+    date: Date.today
+  }
+  end
 
   context "validations" do
     it "is valid with amount, description, category and date parameters" do
-      expense = Expense.new(description: "Bus", amount: 50, category_id: transport_category.id, date: Date.today)
+      expense = Expense.new(valid_params)
       expect(expense).to be_valid
     end
 
     it "is invalid without amount" do
-      expense = Expense.new(description: "Bus", amount: "", category_id: transport_category.id, date: Date.today)
+      expense = Expense.new(valid_params.merge(amount: ""))
       expect(expense).to be_invalid
       expect(expense.errors[:amount]).to include("can't be blank")
     end
 
     it "is invalid if amount is zero" do
-      expense = Expense.new(description: "Bus", amount: 0, category_id: transport_category.id, date: Date.today)
+      expense = Expense.new(valid_params.merge(amount: 0))
       expect(expense).to be_invalid
       expect(expense.errors[:amount]).to include("must be greater than 0")
     end
 
     it "is invalid if amount is less than zero" do
-      expense = Expense.new(description: "Bus", amount: -50, category_id: transport_category.id, date: Date.today)
+      expense = Expense.new(valid_params.merge(amount: -50))
       expect(expense).to be_invalid
       expect(expense.errors[:amount]).to include("must be greater than 0")
     end
 
     it "is invalid without description" do
-      expense = Expense.new(description: "", amount: 50, category_id: transport_category.id, date: Date.today)
+      expense = Expense.new(valid_params.merge(description: ""))
       expect(expense).to be_invalid
       expect(expense.errors[:description]).to include("can't be blank")
     end
