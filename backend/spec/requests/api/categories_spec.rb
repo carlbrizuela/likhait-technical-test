b8bf5e4fced48f92d81547074b4118139a17ef64
carlbrizuela <carljasper.brizuela@gmail.com>
2026-03-05 13:46:20 +0800

    test: add RSPec testing for category model and create method

--- backend/spec/requests/api/categories_spec.rb
+++ backend/spec/requests/api/categories_spec.rb
@@ -15,11 +15,43 @@ RSpec.describe "Api::Categories", type: :request do
       expect(json.map { |c| c["name"] }).to include("Food", "Transport", "Supplies")
     end
 
-    it "returns categories in alphabetical order" do
+    it "returns categories sorted by id" do
       get "/api/categories"
 
       json = JSON.parse(response.body)
-      expect(json.map { |c| c["name"] }).to eq([ "Food", "Supplies", "Transport" ])
+      expect(json.map { |c| c["name"] }).to eq([ "Food", "Transport", "Supplies" ])
+    end
+  end
+
+  describe "POST /api/categories" do
+    context "with valid parameters" do
+      it "creates a new category" do
+        post "/api/categories", params: { category: { name: "Health" } }
+
+        expect(response).to have_http_status(:created)
+        json = JSON.parse(response.body)
+        expect(json["name"]).to eq("Health")
+        expect(Category.exists?(name: "Health")).to be_truthy
+      end
+    end
+
+    context "with invalid paramaters" do
+      it "returns vaildation error if empty string" do
+        post "/api/categories", params: { category: { name: "" } }
+
+        expect(response).to have_http_status(:unprocessable_content)
+        json = JSON.parse(response.body)
+        expect(json["errors"]["name"]).to include("can't be blank")
+      end
+
+      it "returns record not unique error if duplicate" do
+        Category.create!(name: "Leisure")
+        post "/api/categories", params: { category: { name: "Leisure" } }
+
+        expect(response).to have_http_status(:unprocessable_content)
+        json = JSON.parse(response.body)
+        expect(json["errors"]["name"]).to include("Category already exists")
+      end
     end
   end
 end
